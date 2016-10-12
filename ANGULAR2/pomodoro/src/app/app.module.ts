207d86cd102393fdb88fba7599c66662a6e08239
mairanteodoro <mairan.teodoro@gmail.com>
2016-10-12 08:25:09 -0400

    Implement yarn PM

--- ANGULAR2/pomodoro/src/app/app.module.ts
+++ ANGULAR2/pomodoro/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// MaterializeCSS
+import { MaterializeModule } from 'angular2-materialize';
+import "materialize-css";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
