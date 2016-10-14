fd271c2b26b6a0aec65641f506fd056feabc71db
mairanteodoro <mairan.teodoro@gmail.com>
2016-10-14 16:28:36 -0400

    Implement set break and session buttons

--- ANGULAR2/pomodoro/src/app/app.component.ts
+++ ANGULAR2/pomodoro/src/app/app.component.ts
@@ -7,32 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  break:number;
-  session:number;
+  myBreak:number;
+  mySession:number;
+  myTimer:number;
 
   ngOnInit() {
-    this.break = 5
-    this.session = 25
-  }
+    this.myBreak = 5;
+    this.mySession = 25;
+    this.myTimer = this.mySession;
+  };
 
+  // METHODS
   decreaseBreak() {
     console.log("Decrease Break");
-    this.break = this.break === 0 ? 0 : this.break - 1;
-  }
+    this.myBreak = this.myBreak === 0 ? 0 : this.myBreak - 1;
+  };
 
   increaseBreak() {
     console.log("Increase Break");
-    this.break = this.break === this.session ? this.session : this.break + 1;
-  }
+    this.myBreak = this.myBreak === this.mySession ? this.mySession : this.myBreak + 1;
+  };
 
   decreaseSession() {
     console.log("Decrease Session");
-    this.session = this.session === 0 ? 0 : this.session - 1;
-  }
+    this.mySession = this.mySession === 0 ? 0 : this.mySession - 1;
+    this.myTimer = this.mySession;
+  };
 
   increaseSession() {
     console.log("Increase Session");
-    this.session = this.session === 60 ? 60 : this.session + 1;
+    this.mySession = this.mySession === 60 ? 25 : this.mySession + 1;
+    this.myTimer = this.mySession;
+  };
+
+  startSession() {
+    console.log("Start Session");
+  };
+
+  pauseSession() {
+    console.log("Pause Session");
+  };
+
+  resetSession() {
+    console.log("Reset Session");
   }
 
 }
