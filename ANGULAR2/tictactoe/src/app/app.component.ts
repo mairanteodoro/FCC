2fb3937cd23fc1936c833222384e0b8a5fd5b320
mairanteodoro <mairan.teodoro@gmail.com>
2016-11-13 23:51:38 -0500

    Define first layout of the game

--- ANGULAR2/tictactoe/src/app/app.component.ts
+++ ANGULAR2/tictactoe/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  onClick(event) {
+    // console.log(event);
+    console.log("Clicked " + event.target.id);
+  }
+
 }
